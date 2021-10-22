416cfc96aa6843dc25f17a954e869d0e8a47cce3
Joanna Chądzyńska <jmalek87@gmail.com>
2021-10-22 19:51:59 +0200

    init commit

--- App.js
+++ App.js
@@ -1,21 +1,124 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import uploadToAnonymousFilesAsync from "anonymous-files";
+import * as ImagePicker from "expo-image-picker";
+import * as Sharing from "expo-sharing";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+    Image,
+    Platform,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+} from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [selectedImage, setSelectedImage] = useState(null);
+    SplashScreen.preventAutoHideAsync();
+    setTimeout(SplashScreen.hideAsync, 5000);
+    let openImagePickerAsync = async () => {
+        let permissionResult =
+            await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+        if (permissionResult.granted === false) {
+            alert("Permission to access camera roll is required!");
+            return;
+        }
+
+        let pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+        if (pickerResult.cancelled === true) return;
+
+        if (Platform.OS === "web") {
+            let remoteUri = await uploadToAnonymousFilesAsync(pickerResult.uri);
+            setSelectedImage({ localUri: pickerResult.uri, remoteUri });
+        } else {
+            setSelectedImage({
+                localUri: pickerResult.uri,
+                remoteUri: null,
+            });
+        }
+    };
+
+    let openShareDialogAsync = async () => {
+        if (!(await Sharing.isAvailableAsync())) {
+            alert(
+                `The image is available for sharing at: ${selectedImage.remoteUri}`
+            );
+            return;
+        }
+
+        await Sharing.shareAsync(selectedImage.localUri);
+    };
+
+    if (selectedImage !== null) {
+        return (
+            <View style={styles.container}>
+                <Image
+                    source={{ uri: selectedImage.localUri }}
+                    style={styles.thumbnail}
+                />
+                <TouchableOpacity
+                    onPress={openShareDialogAsync}
+                    style={styles.button}>
+                    <Text style={styles.buttonText}>Share this photo</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+    return (
+        <View style={styles.container}>
+            <Image
+                source={{ uri: "https://i.imgur.com/TkIrScD.png" }}
+                style={styles.logo}
+            />
+            <Text style={styles.instructions}>
+                To share a photo from your phone with a friend, just press the
+                button below!
+            </Text>
+
+            <TouchableOpacity
+                onPress={openImagePickerAsync}
+                style={styles.button}>
+                <Text style={styles.buttonText}>Pick a photo</Text>
+            </TouchableOpacity>
+            <StatusBar style='auto' />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+
+    logo: {
+        width: 305,
+        height: 159,
+        marginBottom: 10,
+    },
+    instructions: {
+        color: "#888",
+        fontSize: 18,
+        marginHorizontal: 15,
+        textAlign: "center",
+    },
+    button: {
+        backgroundColor: "blue",
+        padding: 20,
+        borderRadius: 5,
+    },
+    buttonText: {
+        fontSize: 20,
+        color: "#fff",
+    },
+    thumbnail: {
+        width: 300,
+        height: 300,
+        resizeMode: "contain",
+    },
 });
